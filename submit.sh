b5c8ad21ec63d447c3460148e10e91b262bb9003
Dima Granetchi <system.grand@gmail.com>
2017-03-05 00:02:19 +0200

    fix submit script

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r haxelib.zip src haxelib.json README.md LICENCE extraParams.hxml
+zip -r haxelib.zip src haxelib.json README.md LICENSE extraParams.hxml
 haxelib submit haxelib.zip
